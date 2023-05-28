a76e1ea0a8347de26048592b185311b28f21a494
Eric <56614110+EricAndreOrnelasMartinez@users.noreply.github.com>
2023-05-28 09:02:41 -0600

    add

---
+++ ProyectoFinal/Ayuda/ayuda.js
@@ -0,0 +1,22 @@
+let b1 = document.getElementById('preg1')
+let b2 = document.getElementById('preg2')
+let b3 = document.getElementById('preg3')
+let b4 = document.getElementById('preg4')
+let p = document.getElementById('resp')
+
+b1.addEventListener('click', e =>{
+    e.preventDefault()
+    p.innerHTML = " 1.- Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate dolor at porro hic ex, blanditiis, eaque dolore perferendis illo accusamus architecto. Inventore modi tempora corrupti commodi sequi accusamus mollitia harum? Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus rerum perspiciatis in, ut inventore facilis animi harum dolores officiis voluptates, assumenda, atque a esse quo minus. Suscipit reiciendis voluptas architecto."
+})
+b2.addEventListener('click', e =>{
+    e.preventDefault()
+    p.innerHTML = " 2.-Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate dolor at porro hic ex, blanditiis, eaque dolore perferendis illo accusamus architecto. Inventore modi tempora corrupti commodi sequi accusamus mollitia harum? Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus rerum perspiciatis in, ut inventore facilis animi harum dolores officiis voluptates, assumenda, atque a esse quo minus. Suscipit reiciendis voluptas architecto."
+})
+b3.addEventListener('click', e =>{
+    e.preventDefault()
+    p.innerHTML = " 3.-Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate dolor at porro hic ex, blanditiis, eaque dolore perferendis illo accusamus architecto. Inventore modi tempora corrupti commodi sequi accusamus mollitia harum? Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus rerum perspiciatis in, ut inventore facilis animi harum dolores officiis voluptates, assumenda, atque a esse quo minus. Suscipit reiciendis voluptas architecto."
+})
+b4.addEventListener('click', e =>{
+    e.preventDefault()
+    p.innerHTML = " 4.-Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate dolor at porro hic ex, blanditiis, eaque dolore perferendis illo accusamus architecto. Inventore modi tempora corrupti commodi sequi accusamus mollitia harum? Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus rerum perspiciatis in, ut inventore facilis animi harum dolores officiis voluptates, assumenda, atque a esse quo minus. Suscipit reiciendis voluptas architecto."
+})
